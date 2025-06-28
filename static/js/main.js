c26580df2beb10cbca5701dbfe2ac88898acf308
Saeko <saeko514@outlook.jp>
2025-06-28 10:01:50 +0900

    Delete static/js directory

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  liff.init({ liffId: "YOUR_LIFF_ID" })
-    .then(() => {
-      return liff.getProfile();
-    })
-    .then(profile => {
-      console.log("LINEユーザー名", profile.displayName);
-    });
-};
